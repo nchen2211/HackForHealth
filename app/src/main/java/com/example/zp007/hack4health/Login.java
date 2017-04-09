66136864119db21482248338ce2f0bd3b941cfe4
zy-sun <sunzhiyu_44@hotmail.com>
2017-04-08 17:11:47 -0700

    login layout

---
+++ app/src/main/java/com/example/zp007/hack4health/Login.java
@@ -0,0 +1,13 @@
+package com.example.zp007.hack4health;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
